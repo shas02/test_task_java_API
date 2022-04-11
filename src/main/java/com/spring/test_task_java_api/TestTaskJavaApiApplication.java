c3e43b9e0e616f5967ba5ad2acf71448310cb761
Oleksandr Popov <79215934+shas02@users.noreply.github.com>
2022-04-11 19:24:42 +0300

    Initial commit

---
+++ src/main/java/com/spring/test_task_java_api/TestTaskJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.test_task_java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskJavaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskJavaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/test_task_java_api/TestTaskJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.test_task_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskJavaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
